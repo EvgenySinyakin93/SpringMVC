dd2920e3eef97914f7602c2bafda9050e52ccc11
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-06-22 11:41:09 +0300

    first commit

--- src/main/java/org/example/repository/PostRepositoryStubImpl.java
+++ src/main/java/org/example/repository/PostRepositoryStubImpl.java
@@ -1,17 +1,47 @@
 package org.example.repository;
 
+import org.springframework.stereotype.Repository;
+import org.example.exception.NotFoundException;
 import org.example.model.Post;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-public interface PostRepositoryStubImpl {
-    List<Post> all();
+@Repository
+public class PostRepositoryStubImpl implements PostRepository {
+    private final Map<Long, Post> allPosts;
+    private final AtomicLong idCounter = new AtomicLong(0L);
 
-    Optional<Post> getById(long id);
+    public PostRepositoryStubImpl(Map<Long, Post> allPosts) {
+        this.allPosts = new ConcurrentHashMap<>();
+    }
 
-    Post save(Post post);
+    public Collection<Post> all() {
+        return allPosts.values();
+    }
 
-    void removeById(long id);
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(allPosts.get(id));
+    }
 
+    public Post save(Post savePost) {
+        if (savePost.getId() == 0) {
+            long id = idCounter.incrementAndGet();
+            savePost.setId(id);
+            allPosts.put(id, savePost);
+        } else if (savePost.getId() != 0) {
+            Long currentId = savePost.getId();
+            allPosts.put(currentId, savePost);
+        }
+        return savePost;
+    }
+
+    public void removeById(long id) {
+        if (allPosts.containsKey(id)) {
+            allPosts.remove(id);
+        } else {
+            throw new NotFoundException("Введен неверный id");
+        }
+    }
 }
